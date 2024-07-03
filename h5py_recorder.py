e506151dd74d0215161f1971f6a45d67face0ee3
KedarPanchal <kedar.t.panchal@outlook.com>
2024-07-03 15:06:58 -0600

    Delete unneeded files
    Co-authored-by: nike-276 <nike-276@users.noreply.github.com>
    Co-authored-by: sam <samsinghh@users.noreply.github.com>
    Co-authored-by: rohitpochugari <rohitpochugari@users.noreply.github.com>

--- h5py_recorder.py
+++ h5py_recorder.py
@@ -20,6 +20,8 @@ import h5py
 from tf.transformations import quaternion_matrix
 from tf.transformations import euler_from_matrix
 
+import tf_bag
+
 
 
 
@@ -46,35 +48,45 @@ with open(os.path.join(data_dir, 'bag_dict.json')) as f, h5py.File(os.path.join(
 
         uber_rgb_arr = []
         uber_depth_arr = []
-        header_color, header_depth = None, None
+        counter = 0
+
+        bag_transformer = tf_bag.BagTfTransformer(bag)
+
+
+        print(f"Sum: {sum(1 for _ in bag.read_messages())}")
         for topic, msg, t in bag.read_messages():
-            # observations
-            # color
-            if topic == '/camera/color/image_raw/compressed':
-                try:
-                    bridge = CvBridge()
-                    cv_image = bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+            counter += 1
+            if counter % 6 == 0:
+                print(counter)
+                # observations
+                # color
+                if topic == '/camera/color/image_raw/compressed':
+                    try:
+                        bridge = CvBridge()
+                        cv_image = bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+                        cv_image = cv2.resize(cv_image, (320, 180), interpolation=cv2.INTER_AREA)
+                        rgb_arr = np.asarray(cv_image)
+                        uber_rgb_arr.append(rgb_arr)
+                        # cv2.imshow("Color Image", cv_image)
+                        # cv2.waitKey(1)
+
+
+                    except CvBridgeError as e:
+                        rospy.logerr("CvBridge Error: {0}".format(e))
+                # depth
+                elif topic == '/camera/aligned_depth_to_color/image_raw/compressedDepth':
+                    data = np.frombuffer(msg.data[12:], np.uint8)
+                    cv_image = cv2.imdecode(data, cv2.IMREAD_GRAYSCALE)
                     cv_image = cv2.resize(cv_image, (320, 180), interpolation=cv2.INTER_AREA)
-                    rgb_arr = np.asarray(cv_image)
-                    uber_rgb_arr.append(rgb_arr)
-                    cv2.imshow("Color Image", cv_image)
-                    cv2.waitKey(1)
-
-
-                except CvBridgeError as e:
-                    rospy.logerr("CvBridge Error: {0}".format(e))
-            # depth
-            elif topic == '/camera/aligned_depth_to_color/image_raw/compressedDepth':
-                data = np.frombuffer(msg.data[12:], np.uint8)
-                cv_image = cv2.imdecode(data, cv2.IMREAD_GRAYSCALE)
-                cv_image = cv2.resize(cv_image, (320, 180), interpolation=cv2.INTER_AREA)
-                depth_arr = np.asarray(cv_image) # could also just be data but check to be sure
-                uber_depth_arr.append(depth_arr)
-                cv2.imshow("Depth Image", cv_image)
-                cv2.waitKey(1)
+                    depth_arr = np.asarray(cv_image) # could also just be data but check to be sure
+                    uber_depth_arr.append(depth_arr)
+                    # cv2.imshow("Depth Image", cv_image)
+                    # cv2.waitKey(1)
+                    
+                # actions
+                elif topic == '/tf':
+                    pass
                 
-            # actions
-            # TODO  
 
         # creates the datasets
         print(np.array(uber_rgb_arr).shape)
