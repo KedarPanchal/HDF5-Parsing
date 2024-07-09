a06380a01bf4b00cd99283e0dffc2bc9409678d9
KedarPanchal <kedar.t.panchal@outlook.com>
2024-07-09 00:41:09 -0500

    Update mounted directory in docker container

--- docker/run_docker.bash
+++ docker/run_docker.bash
@@ -2,7 +2,7 @@
 
 docker build -t "hdf5_parse" docker/
 if [[ -n $1 ]]; then
-    docker run -v "$(pwd):/usr/src/hdf5_parse" --name "$1" -it "hdf5_parse"
+    docker run -v "$(pwd)/:/usr/share/hdf5_parse" --name "$1" -it "hdf5_parse"
 else
-    docker run -v "$(pwd):/usr/src/hdf5_parse" -it "hdf5_parse"
+    docker run -v "$(pwd)/:/usr/share/hdf5_parse" -it "hdf5_parse"
 fi
