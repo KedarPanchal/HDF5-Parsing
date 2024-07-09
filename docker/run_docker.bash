8233ee9c75e9d6730c158da38197d232e5a0af68
KedarPanchal <kedar.t.panchal@outlook.com>
2024-07-09 00:23:26 -0500

    Add bash script to run docker instance easily

---
+++ docker/run_docker.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker build -t "hdf5_parse" docker/
+if [[ -n $1 ]]; then
+    docker run -v "$(pwd):/usr/src/hdf5_parse" --name "$1" -it "hdf5_parse"
+else
+    docker run -v "$(pwd):/usr/src/hdf5_parse" -it "hdf5_parse"
+fi
